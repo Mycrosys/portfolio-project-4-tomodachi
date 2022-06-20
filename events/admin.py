e2e3ae4c1e2efaf735530c982b60b625c0e1b3b8
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-06-20 08:49:56 +0000

    Feat: add summernote and configure admin panel for easier use

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'category', 'created_on', 'location_time')
+    search_fields = ['title', 'summary']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created_on', 'location_time')
+    summernote_fields = ('summary')
+
