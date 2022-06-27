06403bf46385118afcbfbb97a99bff1e05823d9b
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-06-27 11:09:35 +0000

    Feat: add first version of event details page

--- events/views.py
+++ events/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
+from django.utils.timezone import datetime
 
 
 class EventList(generic.ListView):
     model = Event
-    queryset = Event.objects.order_by('location_time')
+    queryset = Event.objects.order_by('location_time').filter(location_time__time__gte=datetime.now())
     template_name = 'index.html'
     paginate_by = 25
+
+class EventDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects.filter(location_time__time__gte=datetime.now())
+        event = get_object_or_404(queryset, slug=slug)
+        attendee = False
+        if event.attendees.filter(id=self.request.user.id).exists():
+            attendee = True
+
+        return render(
+            request,
+            "event_detail.html",
+            {
+                "event": event,
+                "attendee": attendee
+            },
+        )
+
