301523d645da893cba3351327434199b956d72b1
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-06-27 16:17:47 +0000

    Feat: add navbar indication of current page with own template

--- events/views.py
+++ events/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Event
 from django.utils.timezone import datetime
+from django.utils import timezone
 
 
 class EventList(generic.ListView):
     model = Event
-    queryset = Event.objects.order_by('location_time').filter(location_time__time__gte=datetime.now())
     template_name = 'index.html'
-    paginate_by = 25
+
+    def get_queryset(self):
+        return Event.objects.order_by('location_time').filter(location_time__gt=timezone.now())
+
+
 
 class EventDetail(View):
 
     def get(self, request, slug, *args, **kwargs):
-        queryset = Event.objects.filter(location_time__time__gte=datetime.now())
+        queryset = Event.objects.order_by('location_time').filter(location_time__gt=timezone.now())
         event = get_object_or_404(queryset, slug=slug)
         attendee = False
         if event.attendees.filter(id=self.request.user.id).exists():
