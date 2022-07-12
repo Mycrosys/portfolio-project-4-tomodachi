0eb318a23460a9aeb2db50b4c4eb3be85e3ba7ae
Mycrosys <Ryoga_Hibiki@gmx.de>
2022-07-12 03:10:26 +0000

    Refactor: remove cloudinary from project

--- events/admin.py
+++ events/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Event
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Event
 
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
+    """
+    Basic configuration for an easier to use Admin Panel with Summernote
+    """
 
+    # Displays, Search and Filter for Admin Panel
     list_display = ('title', 'category', 'created_on', 'location_time')
     search_fields = ['title', 'summary']
     list_filter = ('created_on', 'location_time')
+
+    # WYSIWYG edit field for summary of the Event
     summernote_fields = ('summary')
--- tomodachi/settings.py
+++ tomodachi/settings.py
@@ -45,9 +45,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
     'django_summernote',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -142,12 +140,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
